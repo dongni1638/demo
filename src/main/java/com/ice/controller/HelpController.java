f0cf17188b081743dd9bda9eb82cea92e0ff7902
dongni1638 <dongni1638@163.com>
2017-02-27 21:46:42 +0800

    demo提交

---
+++ src/main/java/com/ice/controller/HelpController.java
@@ -0,0 +1,13 @@
+package com.ice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelpController {
+	@RequestMapping(value="help")
+	public String helloWorld(){
+		return "help";
+	}
+
+}
